73fec1c2d83ee33aaf535c12020a41d223974ddf
Hmanshisingh28 <himanshikashyap282005@gmail.com>
2026-01-14 21:45:13 +0530

    add some feature

--- Python_mini_project.py
+++ Python_mini_project.py
@@ -1,7 +1,9 @@
 import random
+# Number guessing
 n=random.randint(1,100)
 a=-1
 guesses=1
+# for comparing the number with user input
 while(a!=n):
     a=int(input("guess the number: "))
     if(a>n):
@@ -10,4 +12,5 @@ while(a!=n):
     elif(a<n):
         print("higher number please")
         guesses+=1
+#finlly when user guesses the number
 print(f"you have guessed the number {n} correctly in {guesses} attempts")
